99ebc70507ccca705404804efd1b84a15980ed1c
alejandro <alejandrodanielaquino@yahoo.com.ar>
2020-03-11 20:18:34 -0300

    Añadida función suma

--- funciones.js
+++ funciones.js
@@ -8,3 +8,7 @@ console.log(saludo(primerUsuario));
 
 //aqui podemos agregar una funcion de agregar a los usuarios para asi poder hacer la factura
 // Agregar un prompt para que el usuario ponga su nombre
+
+function suma(num1, num2) {
+    return num1 + num2;
+}
