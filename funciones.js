83cb2255ea2e0507a53a27f69dadfaa656ce7524
Adrian5730 <Olaveves@gmail.com>
2020-03-11 20:07:13 -0300

    Agrege un comentario en funciones.js

--- funciones.js
+++ funciones.js
@@ -5,3 +5,5 @@ function saludo(usuario) {
 let primerUsuario = "Alejandro";
 
 console.log(saludo(primerUsuario));
+
+//aqui podemos agregar una funcion de agregar a los usuarios para asi poder hacer la factura
